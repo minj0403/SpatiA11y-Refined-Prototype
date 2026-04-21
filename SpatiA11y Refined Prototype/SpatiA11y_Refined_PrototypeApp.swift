339a0f13b87d0d4574173cd7302b83cfe15fc404
mjung040305-debug <mjung040305@gmail.com>
2026-04-21 11:21:41 -0700

    Initial Commit

---
+++ SpatiA11y Refined Prototype/SpatiA11y_Refined_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatiA11y_Refined_PrototypeApp.swift
+//  SpatiA11y Refined Prototype
+//
+//  Created by 정민 on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct SpatiA11y_Refined_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
